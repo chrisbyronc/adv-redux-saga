b282856ec664e60fa7fbc33933c628776f0ed201
Christopher Carter <christopher-carter@chrcart-mbp.lan>
2023-06-02 12:13:14 -0400

    adding solution code

--- day-2/class/01-redux-saga-solution/redux-saga-app/src/sagas.js
+++ day-2/class/01-redux-saga-solution/redux-saga-app/src/sagas.js
@@ -1,15 +1,30 @@
 import { call, put, takeEvery } from 'redux-saga/effects';
-import axios from 'axios';
+
+// Fetch data from the weather API
+const fetchApi = () => {
+  return fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m')
+    .then(response => {
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        throw new Error('Something went wrong on api server!');
+      }
+    })
+    .then(response => {
+      return response;
+    })
+    .catch(error => {
+      throw error;
+    });
+};
 
 function* fetchWeather(action) {
-  try {
-    const response = yield call(axios.get, `https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m`);
-    yield put({ type: 'FETCH_WEATHER_SUCCESS', payload: response.data });
-  } catch (e) {
-    yield put({ type: 'FETCH_WEATHER_FAILURE', message: e.message });
-  }
+  const weatherData = yield call(fetchApi)
+  yield put({ type: 'FETCH_WEATHER_SUCCESS', payload: weatherData})
 }
 
-export function* weatherSaga() {
-  yield takeEvery('FETCH_WEATHER', fetchWeather);
+function* weatherSaga() {
+  yield takeEvery('FETCH_WEATHER', fetchWeather)
 }
+
+export default weatherSaga
--- day-2/class/01-redux-saga-solution/redux-saga-app/src/store.js
+++ day-2/class/01-redux-saga-solution/redux-saga-app/src/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
+import weatherSaga from './sagas';
 
 // Initial state
 const initialState = {
@@ -12,6 +13,12 @@ const initialState = {
 function reducer(state = initialState, action) {
   switch (action.type) {
     // Handle actions here
+    case 'FETCH_WEATHER_SUCCESS':
+      console.log(action.payload)
+      return {
+        ...state,
+        weather: action.payload
+      }
     default:
       return state;
   }
@@ -26,5 +33,6 @@ const store = configureStore({
 });
 
 // Run the saga
+sagaMiddleware.run(weatherSaga)
 
 export default store;
